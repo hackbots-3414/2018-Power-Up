dc60882a8dc18500520e421ae6e237d749fbf71b
Kayleigh <kayleighrf@gmail.com>
2018-02-05 19:15:13 -0500

    Added some fun stuff

--- 2018-PlayForReal/src/org/usfirst/frc/team3414/autonomous/AutonDoNothing.java
+++ 2018-PlayForReal/src/org/usfirst/frc/team3414/autonomous/AutonDoNothing.java
@@ -3,20 +3,16 @@ package org.usfirst.frc.team3414.autonomous;
 public class AutonDoNothing extends AutonBase
 {
 
-	protected void vanillaAuto() {
-		System.out.println("DO NOTHING I SAY!");
-	}
-
 	protected void left() {
-		System.out.println("DO NOTHING I SAY!");		
+		System.out.println("DO NOTHING I SAY! - Hannah and Kayleigh");		
 	}
 
 	protected void center() {
-		System.out.println("DO NOTHING I SAY!");
+		System.out.println("DO NOTHING I SAY! - Hannah and Kayleigh");
 	}
 
 	protected void right() {
-		System.out.println("DO NOTHING I SAY!");
+		System.out.println("DO NOTHING I SAY! - Hannah and Kayleigh");
 	}
 
 }
