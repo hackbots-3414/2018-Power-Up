d6a1a5fea9980d70d9e48a6c63eb3f1e513c3507
Hackbots <Hackbots@10.240.138.231>
2018-03-24 10:45:52 -0400

    New Branch and some center switch things.

--- 2018-PlayForReal/src/org/usfirst/frc/team3414/autonomous/AutonCenterSwitch.java
+++ 2018-PlayForReal/src/org/usfirst/frc/team3414/autonomous/AutonCenterSwitch.java
@@ -4,7 +4,7 @@ import org.usfirst.frc.team3414.sensor.SensorConfig;
 import org.usfirst.frc.team3414.actuators.ActuatorConfig;
 
 public class AutonCenterSwitch extends AutonBase{
-
+//Set the robot right in front of the right side of the switch
 	public AutonCenterSwitch() {
 		super();
 	}
@@ -18,16 +18,17 @@ public class AutonCenterSwitch extends AutonBase{
 	{
 		if("LLL".equals(this.gameData) || "LRL".equals(this.gameData)) 
 		{
-			ActuatorConfig.getInstance().getDrivetrain().motionMagic(3);
-			ActuatorConfig.getInstance().getDrivetrain().turnLeft(.45, 90);
-			ActuatorConfig.getInstance().getDrivetrain().motionMagic(2);
-			ActuatorConfig.getInstance().getDrivetrain().turnRight(.45, 90);
-			ActuatorConfig.getInstance().getDrivetrain().motionMagic(1);
-			ActuatorConfig.getInstance().getLift().setSpeed(-.20);
-			SensorConfig.getInstance().getTimer().waitTimeInMillis(1500);
-			ActuatorConfig.getInstance().getLift().setSpeed(0);
-			ActuatorConfig.getInstance().getMotorIntakeOne().setSpeed(.35);
-			ActuatorConfig.getInstance().getMotorIntakeTwo().setSpeed(.35);
+			
+//			ActuatorConfig.getInstance().getDrivetrain().motionMagic(3);
+//			ActuatorConfig.getInstance().getDrivetrain().turnLeft(.45, 90);
+//			ActuatorConfig.getInstance().getDrivetrain().motionMagic(2);
+//			ActuatorConfig.getInstance().getDrivetrain().turnRight(.45, 90);
+//			ActuatorConfig.getInstance().getDrivetrain().motionMagic(1);
+//			ActuatorConfig.getInstance().getLift().setSpeed(-.20);
+//			SensorConfig.getInstance().getTimer().waitTimeInMillis(1500);
+//			ActuatorConfig.getInstance().getLift().setSpeed(0);
+//			ActuatorConfig.getInstance().getMotorIntakeOne().setSpeed(.35);
+//			ActuatorConfig.getInstance().getMotorIntakeTwo().setSpeed(.35);
 
 			
 //			ActuatorConfig.getInstance().getDrivetrain().movePid(2);
@@ -38,6 +39,23 @@ public class AutonCenterSwitch extends AutonBase{
 		
 		else if("RLR".equals(this.gameData) || "RRR".equals(this.gameData)) 
 		{
+			ActuatorConfig.getInstance().getDrivetrain().goForwardGyro(16, 90);
+			ActuatorConfig.getInstance().getLift().setSpeed(0.3);
+			ActuatorConfig.getInstance().getMotorIntakeAngler().setSpeed(-0.4);
+			SensorConfig.getInstance().getTimer().waitTimeInMillis(1500);
+			ActuatorConfig.getInstance().getLift().setSpeed(0);
+			ActuatorConfig.getInstance().getMotorIntakeAngler().setSpeed(0);
+			ActuatorConfig.getInstance().getMotorIntakeOne().setSpeed(0.5);
+			ActuatorConfig.getInstance().getMotorIntakeTwo().setSpeed(0.5);
+			SensorConfig.getInstance().getTimer().waitTimeInMillis(1500);
+			ActuatorConfig.getInstance().getMotorIntakeOne().setSpeed(0);
+			ActuatorConfig.getInstance().getMotorIntakeTwo().setSpeed(0);
+			
+			
+			
+			
+			
+			
 //			ActuatorConfig.getInstance().getDrivetrain().movePid(2);
 //			ActuatorConfig.getInstance().getDrivetrain().turnRight(0.8, 90);
 //			ActuatorConfig.getInstance().getDrivetrain().movePid(2);
