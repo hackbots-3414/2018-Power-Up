80072e5d26c36d54964e40fe004180d67ce36d16
PhilipLu97 <philiplu97@gmail.com>
2018-08-31 12:03:20 -0400

    Adds declaration of turnRightRadius
    Adds declaration of turnRightRadius to interface (IDriveTrain).

--- 2018-PlayForReal/src/org/usfirst/frc/team3414/actuators/IDriveTrain.java
+++ 2018-PlayForReal/src/org/usfirst/frc/team3414/actuators/IDriveTrain.java
@@ -25,6 +25,15 @@ public interface IDriveTrain {
 	 * @param angle
 	 */
 	public void turnRight(double speed, double angle);
+	/**
+	 * Moves the robot along a circle's circumference, the three radii being the robot's center's and ends' distances from the "origin" 
+	 * @param speed
+	 * @param angle
+	 * @param radius
+	 * @param width
+	 */
+	public void turnRightRadius(double speed, double angle, double radius, double width); 
+	
 	
 
 	/**
